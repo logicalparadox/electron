05c1e157b2b1f148d1b3bb5cabdf1799988526fc
Jake Luer <jake.luer@gmail.com>
2012-05-18 20:48:45 -0400

    example update

--- examples/basic.js
+++ examples/basic.js
@@ -10,10 +10,11 @@ program
   .command('simple')
   .description('A simple task')
   .option('-p, --port [6000]', 'Set the simple port.')
-  .option('-f, --file', 'Use a file', true)
+  .option('-f, --file [path]', 'Use a file', true)
+  .option('-l, --local', 'Flag a local variable')
   .action(function (args) {
-    console.log('simple');
-    console.log(args);
+    console.log(args.mode('l', 'local'));
+    console.log(args.param('p', 'port'));
   });
 
 
